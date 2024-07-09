613ffa5b3dd0da4aac5da0412676cb9512428079
cayto <aidarous1143@gmail.com>
2024-07-10 00:13:16 +0300

    correct the url link

--- web/config/config.js
+++ web/config/config.js
@@ -1,5 +1,5 @@
 // export const URL = 'http://localhost:9000/api/v1';
 // export const uploadFolder = 'http://localhost:9000/uploads';
 
-export const URL = 'hhttps://renthousems.onrender.com/api/v1';
+export const URL = 'https://renthousems.onrender.com/api/v1';
 export const uploadFolder = 'https://renthousems.onrender.com/uploads';
