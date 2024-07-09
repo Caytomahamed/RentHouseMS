cfce0ad40e5629fb9375df7152bfc879365c0a81
cayto <aidarous1143@gmail.com>
2024-07-10 00:06:50 +0300

    delete react-push packages

--- web/src/components/Notification/PushNotification.jsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-
-const PushNotification = () => {
-  return <div>PushNotification</div>;
-};
-
-export default PushNotification;
